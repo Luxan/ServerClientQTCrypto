c212bbd1565f8b5bd53aef90a1c6ca0410e84cbc
Luxan <luxawes@gmail.com>
2016-09-14 10:11:11 +0200

    Update buffer.h

--- include/shared/buffer.h
+++ include/shared/buffer.h
@@ -4,65 +4,55 @@
 #include <stdio.h>
 #include <string.h>
 #include <string>
+//!declare how much data can be stored in buffer. uint16_t = max 65536 bytes
+#define BUFF_SIZE uint16_t
 
-
+/*!
+\class Buffer 
+\memory clears all data that stored inside
+\brief class used to store byte data. Can store max 65536 bytes(depends on size of BUFF_SIZE)
+*/
 class Buffer
 {
 protected:
-    uint8_t *buff;
-    uint16_t length;
+    uint8_t *buff; 	/*!pointer to data buffer*/
+    BUFF_SIZE length;	/*!length of data buffer*/
 
 public:
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
-	*/
-    Buffer(uint8_t *_buff, uint8_t _length):
+    /*!
+    \brief Constructor to store data inside class object
+    \param _buff - pointer to data that will be stored
+    \param _length - length of data that will be stored
+    */
+    Buffer(uint8_t *_buff, BUFF_SIZE _length):
         buff(_buff),length(_length)
     {}
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
-	*/
+    /*!
+    \return uint8_t * - pointer to data buffer
+    \brief returns pointer to data buffer stored inside class object
+    */
     virtual uint8_t * getPointerToBuffer() const
     {
         return buff;
     }
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
-	*/
+    /*!
+    \return size of data
+    \brief returns size of data buffer stored inside class object
+    */
     virtual size_t getLength() const
     {
         return length;
     }
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
-	*/
+    /*!
+    \brief destructor that destroy all data stored inside class object
+    */
     virtual ~Buffer()
     {
-		if (buff != nullptr)
-			delete[] buff;
-	}
+        if (buff != nullptr)
+        delete[] buff;
+    }
 };
 
