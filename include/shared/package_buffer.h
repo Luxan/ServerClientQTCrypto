36169f7b2138c51a7eff8857325b36c51beaafe0
Luxan <luxawes@gmail.com>
2016-09-14 17:52:51 +0200

    Update package_buffer.h

--- include/shared/package_buffer.h
+++ include/shared/package_buffer.h
@@ -2,79 +2,61 @@
 
 #include "buffer.h"
 
+/*!
+\class PackageBuffer
+\brief store bytes of package used in network communication.
+*/
 class PackageBuffer : public Buffer
 {
 private:
-	uint8_t offset;
+	BUFF_SIZE offset; /*! offset is increased if fillBuffer was called to make filled bytes inaccessible */
 
 public:
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
+	/*!
+	\brief give length of buffer with offset stored inside of object of class
+	\return length of buffer with offset.
 	*/
 	virtual size_t getLength() const;
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
+	/*!
+	\brief give pointer to buffer with offset stored inside of object of class
+	\return pointer to buffer with offset(if fillBuffer was called then offset will be increased so given data for filling will be unaccessible).
 	*/
 	virtual uint8_t * getPointerToBuffer() const;
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
+	/*!
+	\brief COPY data stored in object class to given buffer and increase offset of data that is stored inside object class
+	\param _buff - pointer to buffer to fill
+	\param size - length of buffer to fill
+	\throw "Not enough memory in PackageBuffer" if stored data length is smaller than given to fill
+	\pre length of initial data length must be equal or more than length of given to fill
+	\example if PackageBuffer stored 0123456789 and fillBuffer called to fill buffer with length 5 it fills 01234. 
+	If next time fillBuffer will be called to fill buffer with length 5 it will fill 56789.
 	*/
-	void fillBuffer(uint8_t *_buff, size_t size);
+	void fillBuffer(uint8_t *_buff, BUFF_SIZE size);
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
+	/*!
+	\brief COPY and store data inside object class
+	\param _buff - pointer to buffer to store
+	\param _length - length of buffer to store
 	*/
-	PackageBuffer(const uint8_t *_buff, uint8_t _length);
+	PackageBuffer(const uint8_t *_buff, BUFF_SIZE _length);
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
+	/*!
+	\brief Destructor that destroy all data stored inside object class
 	*/
 	virtual ~PackageBuffer();
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
+	/*!
+	\param _buff - pointer to buffer to concantenate with
+	\param _length - length of buffer to concantenate with
+	\brief create new buffer and copy initial data stored in object class and received buffer as parameter.
 	*/
-	void concatBuff(uint8_t *_buff, uint8_t _length);
+	void concatBuff(uint8_t *_buff, BUFF_SIZE _length);
 
-	/**
-	\param
-	\return
-	\throw
-	\brief
-	\pre
-	\post
+	/*!
+	\return pointer to new copy of actual object.
+	\brief create new object of a class and copy all data to it then erase all data in self object. Return created copy of an object. 
 	*/
 	PackageBuffer *copyToNewAndClear();
 };
