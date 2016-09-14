a448d7c5d6a29df951bcdb749b33f6f9ae192213
Luxan <luxawes@gmail.com>
2016-09-14 18:02:04 +0200

    Update error_enum.h

--- include/shared/error_enum.h
+++ include/shared/error_enum.h
@@ -2,10 +2,14 @@
 
 #include <stdint.h>
 
+/*!
+\enum eError
+\brief enum that defines errors in system.
+*/
 enum eError : uint8_t
 {
-    Undefined
-    ,ServerIsAnaviable
-    ,WrongLoginOrPassword
+    Undefined /*! Undefined error. */
+    ,ServerIsAnaviable /*! Error that identify that server is unreacjable. */
+    ,WrongLoginOrPassword /*! Error that suggest that entered login or password are incorrect. */
 };
 
