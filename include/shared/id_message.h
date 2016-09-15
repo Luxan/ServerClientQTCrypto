070e3046fa269582ba029f1cd3a30ed2a5fb11cd
Luxan <luxawes@gmail.com>
2016-09-15 16:33:31 +0200

    Update id_message.h

--- include/shared/id_message.h
+++ include/shared/id_message.h
@@ -1,41 +1,32 @@
 #pragma once
 
 #include "../shared/id_client.h"
-
+/*!
+\class MessageID
+\brief class that is used to identify message. It contains original number for every message. 
+*/
 class MessageID : public ClientID
 {
 public:
-    /**
-    \param
-    \return
-    \throw
-    \brief
-    \pre
-    \post
+    /*!
+    \brief copy constructor to copy original id of given object to new object
+    \param c - reference of object which id will be copyed to new object 
     */
     MessageID(const MessageID &c):
         ClientID(c)
     {}
 
-    /**
-    \param
-    \return
-    \throw
-    \brief
-    \pre
-    \post
+    /*!
+    \brief cast operator from uint32_t to MessageID
+    \param _id - original id of message
+    \return object of MessageID with given original id
     */
     MessageID(uint32_t _id):
         ClientID(_id)
     {}
 
-    /**
-    \param
-    \return
-    \throw
-    \brief
-    \pre
-    \post
+    /*!
+    \brief default contructor that initialize original id with static number staticID
     */
     MessageID():
         ClientID()
