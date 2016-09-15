37ea9ea329d2f13b1ab85feef203c0f8e796c174
Luxan <luxawes@gmail.com>
2016-09-15 16:57:19 +0200

    Update status.h

--- include/shared/status.h
+++ include/shared/status.h
@@ -6,15 +6,15 @@
 #include <stdint.h>
 
 /**
-\class
-\brief
+\class enum Status
+\brief shows status of user.
 */
 enum class Status : uint8_t
 {
-    Offline,
-    Away,
-    Sneezy,
-    Busy,
-    MissYou,
-    Online
+    Offline, /*! if user is not logged in */
+    Away, /*! if user is logged in but inactive for a while */
+    Sneezy, /*! if user is logged in but inactive for a long time */
+    Busy, /*! if user is not logged in but forsedhis status to busy */
+    MissYou, /*! if user is not logged in and send notice to someone to contact him asap*/
+    Online /*! if user is logged in and is active */
 };
